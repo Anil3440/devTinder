ce7356601eb121078443293de3af5585b22e9a2b
Anil <cadetanil40@gmail.com>
2025-11-09 00:58:55 +0530

    multiple route handlers

--- src/app.js
+++ src/app.js
@@ -1,30 +1,16 @@
 const express = require('express');
 
 const app = express();
-//This will match all HTTP method Api calls to /test.
-app.use("/test",(req,res)=>{
-    res.send("Welcome to home page.");
-});
+//multiple route handlers
+app.use("/user",(req,res,next)=>{
+    console.log("Route handler 1");
+    next();
+    // res.send("response 1");//throws error since res is already send for req by url.
+},(req,res)=>{
+    console.log("Route handler 2");
+    res.send("response from handler 2");
+})//can add as many route handlers as we want but have to send res in the end.
 
-//This will only match GET Api calls to /user.
-app.get("/user",(req,res)=>{
-    
-    res.send({firstName:"Anil",lastName:"Choudhary"});
-})
-app.post("/user",(req,res)=>{
-    //Save data to DB code
-    res.send("Data saved successfully to DB");
-})
-app.delete("/user",(req,res)=>{
-    res.send("user deleted successfully");
-})
-
-app.get("/abc/:userid/:username",(req,res)=>{//ab+b->b can occur 1 or more times,ab?c->b can occur 0 or 1 time,ab*c->b can occur 0 or more times.
-    //had to put the string in /abc/ -> Regex because if i use string it throws error for advance routing methods like *,?,+.
-    // console.log(req.query);
-    console.log(req.params);
-    res.send({firstName:"Anil",lastName:"Choudhary"});//   /.*fly$/->anything ending with fly works.
-})
 
 
 app.listen(3440,()=>{
