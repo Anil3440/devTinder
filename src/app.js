210b7b5dbbb68e135f2b309ba35156ae8c181802
Anil <cadetanil40@gmail.com>
2025-11-09 11:43:49 +0530

    implement user and admin authentication middleware

--- src/app.js
+++ src/app.js
@@ -1,18 +1,23 @@
 const express = require('express');
-
+const {adminAuth,userAuth} = require("./middlewares/adminAuth.js");
 const app = express();
-//multiple route handlers(X) | multiple middlewares and one route handler which actuaally handles the route req(✓).
-app.use("/user",(req,res,next)=>{
-    console.log("Route handler 1");//middleware
-    next();
-    // res.send("response 1");//throws error since res is already send for req by url.
-},(req,res)=>{
-    console.log("Route handler 2");
-    res.send("response from handler 2");//route handler
-})//can add as many middlewares as we want but have to send res in the end.
 
-
-
+//handling auth middleware for all requests GET,POST,DELETE,PATCH,PUT by using app.use();
+//actual use of middleware if writing single authentication code for all routes.
+// app.use("/admin",adminAuth);
+// app.use("/user",userAuth);
+app.use("/user/data",userAuth,(req,res)=>{//can directly pass middleware here if only 1 route is there or pass in every route handler.its checks auth first then goes to callBack func.
+    res.send("user data sent");
+});
+app.use("/user/profile",userAuth,(req,res)=>{
+    res.send("user profile shown");
+})
+app.use("/admin/getAllData",adminAuth,(req,res)=>{
+    res.send("sent all data");
+});
+app.use("/admin/deleteUserData",adminAuth,(req,res)=>{
+    res.send("user data deleted");
+})
 app.listen(3440,()=>{
     console.log("server is currently listening on port 3440");
 });
