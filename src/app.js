2b55e59dc7d3a1709b0c05a851efac6cc944fdee
Anil <cadetanil40@gmail.com>
2025-11-09 10:44:49 +0530

    multiple middlewares and final req handler

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 
 const app = express();
-//multiple route handlers
+//multiple route handlers(X) | multiple middlewares and one route handler which actuaally handles the route req(✓).
 app.use("/user",(req,res,next)=>{
-    console.log("Route handler 1");
+    console.log("Route handler 1");//middleware
     next();
     // res.send("response 1");//throws error since res is already send for req by url.
 },(req,res)=>{
     console.log("Route handler 2");
-    res.send("response from handler 2");
-})//can add as many route handlers as we want but have to send res in the end.
+    res.send("response from handler 2");//route handler
+})//can add as many middlewares as we want but have to send res in the end.
 
 
 
