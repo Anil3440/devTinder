a037c94a8826b94e55e7129e2e873ec67f0361c8
Anil <cadetanil40@gmail.com>
2025-11-11 22:12:57 +0530

    GET-POST-PATCH-DELETE taking input from user through api

--- src/app.js
+++ src/app.js
@@ -17,6 +17,80 @@ app.post("/signup",async (req,res)=>{
     }
     
 })
+// fetch user info using email
+app.get("/user",async(req,res)=>{
+    const userEmail = req.body.emailId;
+    try{
+        const users = await User.find({emailId:userEmail});
+        if(users.length===0){
+            res.status(400).send("no user found");
+        }else{
+            res.send(users);
+        }
+    }
+    catch(err){
+        res.status(400).send("something went wrong"+err.message);
+    }
+
+});
+//feed API - GET /feed - get all of the users from the database
+app.get("/feed",async(req,res)=>{
+    try{
+        const users = await User.find({});
+        if(users.length===0){
+            res.send("no users to show");
+        }else{
+            res.send(users);
+        }
+    }
+    catch(err){
+        res.send("something went wrong");
+    }
+})
+//findOne API - GET /find - get one object from DB.
+app.get("/find",async(req,res)=>{
+    const userEmail = req.body.emailId;
+    try{
+        const user = await User.findOne({
+            emailId: userEmail,
+            age: {$gte: 21}
+        });
+        res.send(user);
+        // if(!user){
+        //     res.send("no data found");
+        // }
+        // else{
+        //     res.send(user);
+        // }
+    }
+    catch(err){
+        res.send("something went wrong");
+    }
+})
+//delete user from DB
+app.delete("/user",async(req,res)=>{
+    const userId = req.body._id;
+    try{
+        const user = await User.findByIdAndDelete(userId);
+        res.send("user deleted successfully");
+    }
+    catch(err){
+        res.status(400).send("something went wrong");
+    }
+})
+//update data of the user
+app.patch("/user",async(req,res)=>{
+    const userId = req.body._id;
+    const data = req.body;
+    try{
+        const user = await User.findByIdAndUpdate(userId,data,{returnDocument: "after"});
+        console.log(user);
+        res.send("user data updated successfully");
+    }
+    catch(err){
+        res.send("something went wrong");
+    }
+})
 
 connectDB()
     .then(()=>{
